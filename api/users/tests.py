a115fe89891f53caea81455274e2d31e49a661b3
Bakley <barclaykoin@gmail.com>
2019-03-05 10:47:23 +0300

    [finishes #164275560]Add Travis and maintanability tool

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from users.models import User
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    """Dummy test case for user"""
+
+    def test_user_was_saved(self):
+        """Dummy"""
+        user = User.objects.create(name="Brian", nick_name="Ogutu",
+                                   email="codingbrian58@gmail.com",
+                                   password="Henkdebruin58")
+        self.assertEqual(user.name, "Brian")
+        self.assertEqual(user.email, "codingbrian58@gmail.com")
+
+
+class YourTestClass(TestCase):
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
