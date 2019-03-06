4699ea5dffd75dbe5ce095dac1e6d2dccefca0a9
Abraham Kamau <kirumba.kamau@gmail.com>
2019-03-06 04:15:39 +0300

    [feature #164361710] Created tests for user login authentication

--- api/config/settings.py
+++ api/config/settings.py
@@ -13,7 +13,7 @@ import datetime
 import logging
 import environ
 from django.utils.translation import gettext_lazy as _
-import os
+import os, datetime
 
 # Project Base Paths
 # project_root/api/config/settings.py - 3 = project_root/
@@ -109,6 +109,43 @@ REST_FRAMEWORK = {
     ),
     'EXCEPTION_HANDLER':
     'config.exceptions.api_exception_handler',
+}
+
+# JWT authentication settings
+
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_AUTH_COOKIE': None,
 }
 
 # Djoser Auth Related Settings
@@ -361,6 +398,9 @@ MOBILE_ACTIVATION_TOKEN_LENGTH = 6
 MOBILE_ACTIVATION_TOKEN_HASH_ALGORITHM = 'sha256'
 
 # Test Settings
+# TEST_PAYLOAD_PATH = str(API_DIR) + '/utils/test/'
+# TEST_DATA_PATH = TEST_PAYLOAD_PATH + 'data/'
+# TEST_RUNNER = 'utils.test.test_runner.CMTestRunner'
 
 # Site Reliability Team
 # https://docs.djangoproject.com/en/2.0/ref/settings/#admins
--- api/users/tests.py
+++ api/users/tests.py
@@ -1,4 +1,12 @@
+import json
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
+from rest_framework.test import APIClient, APITestCase
+
 from users.models import User
 
 # Create your tests here.
@@ -21,3 +29,54 @@ class YourTestClass(TestCase):
     def test_one_plus_one_equals_two(self):
         print("Method: test_one_plus_one_equals_two.")
         self.assertEqual(1 + 1, 2)
+"""
+Tests for user Auth
+"""
+
+
+
+User = get_user_model()
+
+class BaseTest(APITestCase):
+    """
+    The base test class for all default test case settings
+    """
+    client = APIClient
+
+    def login_user(self, email="", password=""):
+        url = reverse("user_login")
+        return self.client.post(
+            url,
+            data=json.dumps({
+                "email": email,
+                "password": password
+            }),
+            content_type="application/json"
+        )
+
+    def setUp(self):
+        
+        self.user = User.objects._create_user(
+            nick_name = "admin",
+            email = "admin@questioner.com",
+            password = "@Admin123"
+        )
+
+class LoginTest(BaseTest):
+    """
+    Tests for auth/login endpoint
+    """
+    def test_login_correct_credentials(self):
+        """
+        Test correct user login credentials
+        """
+        response = self.login_user("admin@questioner.com", "@Admin123")
+        self.assertIn("token", response.data)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+    
+    def test_login_incorrect_credentials(self):
+        """
+        Test incorrect user login credentials
+        """
+        response = self.login_user("abraham", "aBu#123")
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
