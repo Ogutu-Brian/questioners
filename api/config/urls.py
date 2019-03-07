adcbf8eec63fbf397d902e31adc60b153cf297ea
IssaIan <issamwangi@gmail.com>
2019-03-08 02:14:58 +0300

    [feature #164361710] Add social oauth2 url

--- api/config/urls.py
+++ api/config/urls.py
@@ -44,6 +44,8 @@ meetup_urls = include('meetups.urls')
 oauth2_urls = include('rest_framework_social_oauth2.urls')
 
 meetup_urls = include('meetups.urls')
+oauth2_urls = include('rest_framework_social_oauth2.urls')
+
 urlpatterns = [
     path('auth/', oauth2_urls),
     path('api/', doc_urls),
