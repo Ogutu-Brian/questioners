a579f238b6b7377707cf1d6d24575173ac69a159
Abraham Kamau <kirumba.kamau@gmail.com>
2019-03-08 01:59:04 +0300

    [feature #164361710] tests for google oauth

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include
 from django.urls import path
 from .views import (SignUpView, ResendActivationView, ActivationView,
                     LoginView, LogoutView, ChangePasswordView, ChangeEmailView,
@@ -9,6 +10,7 @@ urlpatterns = [
     path('resend/', ResendActivationView.as_view(), name='user_resend'),
     path('activate/', ActivationView.as_view(), name='user_activate'),
     path('login/', LoginView.as_view(), name='user_login'),
+    path('oauth/', include('social_django.urls', namespace='social')),
     path('logout/', LogoutView.as_view(), name='user_logout'),
     path('change_password/', ChangePasswordView.as_view(),
          name='change_password'),
--- api/users/tests.py
+++ api/users/tests.py
@@ -1,11 +1,16 @@
-import json
+import json, responses, re
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
+
+from contextlib import contextmanager
+
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
 from rest_framework.test import APIClient, APITestCase
+
+from urllib.parse import parse_qs, urlparse
 
 from users.models import User
 
@@ -33,15 +38,25 @@ class YourTestClass(TestCase):
 Tests for user Auth
 """
 
-
-
 User = get_user_model()
+user_data = {
+    'id': '001',
+    'name': 'Abraham Kamau',
+    'email': 'ericabraham806@gmail.com',
+}
 
 class BaseTest(APITestCase):
     """
     The base test class for all default test case settings
     """
     client = APIClient
+    provider = 'google-oauth2'
+    QUERY_STRINGS_RE = '\?([\w-]+(=[\w-]*)?(&[\w-]+(=[\w-]*)?)*)?$'
+
+    base_url = 'https://www.googleapis.com/plus/v1/people/me'.replace('.', r'\.')
+    mock_url = re.compile(
+        base_url + QUERY_STRINGS_RE
+    )
 
     def login_user(self, email="", password=""):
         url = reverse("user_login")
@@ -52,6 +67,22 @@ class BaseTest(APITestCase):
                 "password": password
             }),
             content_type="application/json"
+        )
+    def save_user(self):
+        User.objects.create(
+            name="Abraha Kamau",
+            nick_name="greean",
+            email="ericabraham806@gmail.com",
+            password="@Us3r.com"
+            )
+    def social_try_token(self, token):
+        """
+        Method to enable simple means for access via token
+        """
+        social_url = "/api/auth/oauth/{}/"
+        return self.client.post(
+            social_url.format(self.provider),
+            data={"access_token": token},
         )
 
     def setUp(self):
@@ -80,3 +111,69 @@ class LoginTest(BaseTest):
         """
         response = self.login_user("abraham", "aBu#123")
         self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
+def generate_resp(request):
+    token = parse_qs(urlparse(request.url).query)['access_token'][0]
+    status = 200
+    try:
+        body = user_data[token]
+    except KeyError:
+        body = {'errors': 'Invalid Token'}
+        status = 401
+    return (status, {}, json.dumps(body))
+    
+@contextmanager
+def mocked(endpoint):
+    with responses.RequestsMock() as rsps:
+        rsps.add_callback(responses.GET, endpoint,
+                          callback=generate_resp,
+                          content_type='application/json',
+                          match_querystring=True,
+                          )
+        yield rsps
+
+class GoogleOAuthTest(BaseTest):
+    """
+    Tests for google authentication using OAuth2
+    """
+
+    def test_new_user_creation(self):
+        "Ensure that we can correctly create a new user for someone with a valid token."
+        for token, data in user_data.items():
+            with self.subTest(token=token), mocked(self.mock_url):
+                response = self.social_try_token(token)
+
+                self.assertEqual(response.status_code, HTTP_201_CREATED)
+                self.assertIn('token', response.data)
+                self.assertNotEqual(response.data['token'], token)
+                self.assertEqual(User.objects.filter(email=data['email']).count(), 1)
+                
+                user_model = User.objects.get(email=data['email'])
+                self.assertEqual(user_model.username, user_model.email)
+
+    def test_existing_user_login(self):
+        """
+        Tests if existing user can login with token
+        """
+        for token, data in user_data.items():
+            self.save_user
+            with self.subTest(token=token), mocked(self.mock_url):
+                response = self.social_try_token(token)
+                self.assertEqual(response.status_code, HTTP_200_OK)
+                self.assertIn('token', response.data)
+                self.assertNotEqual(response.data['token'], token)
+                self.assertEqual(User.objects.filter(email=data['email']).count(), 1)
+                user = User.objects.get(email=data['email'])
+                self.assertEqual(user.get_full_name(), data['name'])
+
+    def test_invalid_google_token(self):
+        "Ensure that users who present an invalid social token are not granted access"
+        emails = {u.email for u in User.objects.all()}
+
+        token = 'invalid_token'
+        response = self.social_try_token(token)
+
+        self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
+        self.assertNotIn('token', response.data)
+
+        new_emails = {u.email for u in User.objects.all()}
+        self.assertEqual(emails, new_emails)
