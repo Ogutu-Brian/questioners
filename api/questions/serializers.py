1eacf5659d9c1f91648fb5e0a14aa4950f2678f9
Brian <codingbrian58@gmail.com>
2019-03-20 17:26:51 +0300

    Return user details on resources (#26)

--- api/questions/serializers.py
+++ api/questions/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from questions.models import Question
+from users.models import User
+from users.serializers import FetchUserSerializer
+
 
 class QuestionsSerializer(serializers.ModelSerializer):
     """
@@ -11,11 +14,12 @@ class QuestionsSerializer(serializers.ModelSerializer):
             'title', 'body'
         )
 
+
 class ViewQuestionsSerializer(serializers.ModelSerializer):
     """
     Serializer class for fetching all questions specific to that meetup
     """
+    created_by = FetchUserSerializer()
     class Meta:
         model = Question
         fields = '__all__'
-
