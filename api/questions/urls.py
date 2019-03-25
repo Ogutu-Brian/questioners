4af9f77a62949873b855c8f85e84dd605919e9a3
Amos wachira <amoswachira50@gmail.com>
2019-03-25 10:24:58 +0300

    bg(view one question): view specific question for a specific meetup
    - add view specific question
    - change url pattern for rsvp
    
    [finish #164869458]

--- api/questions/urls.py
+++ api/questions/urls.py
@@ -4,7 +4,7 @@ Urls for questions Epic
 
 from django.urls import path
 from questions.views import (QuestionViews, ViewQuestionsView,
-                             UpvoteQuestion, DownvoteQuestion, QuestionEditViews)
+                             UpvoteQuestion, DownvoteQuestion, QuestionEditViews, ViewSpecificQuestionView)
 urlpatterns = [
     path('<str:id>/questions/', QuestionViews.as_view(), name='question'),
     path('<str:id>/questions', ViewQuestionsView.as_view(), name='view_questions'),
@@ -16,4 +16,6 @@ urlpatterns = [
          QuestionEditViews.as_view(), name='delete_question'),
     path('<str:m_id>/questions/<str:id>/',
          QuestionEditViews.as_view(), name='edit_question'),
+    path('<str:m_id>/questions/<str:id>',
+         ViewSpecificQuestionView.as_view(), name='specific_question'),
 ]
